d925ef2a51fbc65e2bdfc21f86a477553a0b4f12
Tim Mushrush <tmush92@gmail.com>
2022-06-09 16:47:16 -0500

    removed __pycache__ for .gitignore later

---
+++ src/dnd/dessarin_valley_encounters.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import random
+
+
+# Early Travels
+# Day
+dessarin_valley_early_day = pd.DataFrame()
+dessarin_valley_early_day['roll'] = list(range(19))
+dessarin_valley_early_day['roll'] = dessarin_valley_early_day['roll']+2
+dessarin_valley_early_day['encounter'] = [
+    'Aarakocra scouts', 'Knights of Samular', 'Pilgrims', 'Elk Tribe Hunters',
+    '1d3 ankhegs', '1d3 + 1 bugbears', '1d4 + 1 orcs', 'Dwarf miners', 'Caravan',
+    'Homestead', 'Air cult scouts', 'Water cult marauders', 'Earth cult robbers', 'Fire cult raiders',
+    '1d4 + 1 gnolls', 'Shepherds', '1d6 + 2 wolves', '1d3 ogres', '1d2 perytons']
+dessarin_valley_early_day
+# Night
+dessarin_valley_early_night = pd.DataFrame()
+dessarin_valley_early_night['roll'] = list(range(19))
+dessarin_valley_early_night['roll'] = dessarin_valley_early_night['roll']+2
+dessarin_valley_early_night['encounter'] = [
+    '1d4 + 1 jackalweres', 'Pilgrims', '1d2 owlbears', 'Elk Tribe Hunters',
+    '1d3 ankhegs', '1d3 + 1 bugbears', '1d4 + 1 orcs', 'Air cult scouts', 'Water cult marauders',
+    'Earth cult robbers', 'Fire cult raiders', '1d4 + 1 gnolls', '1d6 + 2 wolves', '1d3 ogres',
+    '1d2 gargoyles', '1d3 + 1 ghouls', '1d2 perytons', '1d3 wights', 'The Watchful Knight']
+dessarin_valley_early_night
+
+# Later Travels
+# Day
+
+# Night
+
+# River Travels
