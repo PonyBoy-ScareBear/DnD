0e23496648c5c21f3abbbf84bc8f0edefa4e7757
Tim Mushrush <tmush92@gmail.com>
2022-06-09 09:43:19 -0500

    split into two separate calculators

---
+++ src/dnd/level_calc.py
@@ -0,0 +1,50 @@
+def lvl_calc(xp: int = 0) -> int:
+    """Calculates level using experience points.
+
+    Args:
+        xp (int, optional): Experience Points. Defaults to 0.
+
+    Returns:
+        level (int): Level
+    """
+    if xp < 300:
+        level = 1
+    elif xp < 900:
+        level = 2
+    elif xp < 2700:
+        level = 3
+    elif xp < 6500:
+        level = 4
+    elif xp < 14000:
+        level = 5
+    elif xp < 23000:
+        level = 6
+    elif xp < 34000:
+        level = 7
+    elif xp < 48000:
+        level = 8
+    elif xp < 64000:
+        level = 9
+    elif xp < 85000:
+        level = 10
+    elif xp < 100000:
+        level = 11
+    elif xp < 120000:
+        level = 12
+    elif xp < 140000:
+        level = 13
+    elif xp < 165000:
+        level = 14
+    elif xp < 195000:
+        level = 15
+    elif xp < 225000:
+        level = 16
+    elif xp < 265000:
+        level = 17
+    elif xp < 305000:
+        level = 18
+    elif xp < 355000:
+        level = 19
+    else:
+        level = 20
+    return level
--- src/dnd/prof_bonus_calc.py
+++ src/dnd/prof_bonus_calc.py
@@ -1,57 +1,13 @@
-def lvl_calc(xp: int = 0) -> int:
-    """Calculates level using experience points.
+def prof_calc(xp: int = 0) -> int:
+    """Calculates proficiency bonus using experience points.
 
     Args:
         xp (int, optional): Experience Points. Defaults to 0.
 
     Returns:
-        level (int): Level
+        prof_bonus (int): Proficiency Bonus.
     """
     if xp < 300:
-        level = 1
-    elif xp < 900:
-        level = 2
-    elif xp < 2700:
-        level = 3
-    elif xp < 6500:
-        level = 4
-    elif xp < 14000:
-        level = 5
-    elif xp < 23000:
-        level = 6
-    elif xp < 34000:
-        level = 7
-    elif xp < 48000:
-        level = 8
-    elif xp < 64000:
-        level = 9
-    elif xp < 85000:
-        level = 10
-    elif xp < 100000:
-        level = 11
-    elif xp < 120000:
-        level = 12
-    elif xp < 140000:
-        level = 13
-    elif xp < 165000:
-        level = 14
-    elif xp < 195000:
-        level = 15
-    elif xp < 225000:
-        level = 16
-    elif xp < 265000:
-        level = 17
-    elif xp < 305000:
-        level = 18
-    elif xp < 355000:
-        level = 19
-    else:
-        level = 20
-    return level
-
-
-def prof_calc(xp: int = 0) -> int:
-    if xp < 300:
         proficiency_bonus = 2
     elif xp < 900:
         proficiency_bonus = 2
@@ -91,4 +47,5 @@ def prof_calc(xp: int = 0) -> int:
         proficiency_bonus = 6
     else:
         proficiency_bonus = 6
-    return proficiency_bonus
+    prof_bonus = proficiency_bonus
+    return prof_bonus
