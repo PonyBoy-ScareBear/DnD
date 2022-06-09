d19ce601ef25b83697adfbf412178282596cda48
Tim Mushrush <tmush92@gmail.com>
2022-06-09 10:09:49 -0500

    initial commit

---
+++ src/dnd/random_integer_generator.py
@@ -0,0 +1,14 @@
+import random
+
+
+def rand_int_gen(number_out_of: int = 20) -> int:
+    """Generates random number from range of numbers.
+
+    Args:
+        number_out_of (int, optional): The end of the range of numbers. Defaults to 20.
+
+    Returns:
+        int: Random number from one to whatever was specified.
+    """
+    integer = random.randint(1,number_out_of)
+    return integer
