7e0ab2a5631fe6a87861b27ee1c16d0efc29fdae
Tim Mushrush <tmush92@gmail.com>
2022-06-09 12:57:25 -0500

    update single_roll()

--- src/dnd/dice_roller.py
+++ src/dnd/dice_roller.py
@@ -1,18 +1,17 @@
 import random
 
 
-def single_roll(type_of_die: int = 20) -> tuple(int, int):
+def single_roll(type_of_die: int = 20) -> int:
     """Simulates rolling a single die.
 
     Args:
         type_of_die (int, optional): Type of die that is being rolled. Defaults to 20.
 
     Returns:
-        type_of_die (int): Type of die that was rolled.
         roll (int): The result of the die roll.
     """
     roll = random.randint(1, type_of_die)
-    return type_of_die, roll
+    return roll
 
 
 def multiple_rolls(number_of_dice: int, type_of_dice: int = 6) -> list[int]:
